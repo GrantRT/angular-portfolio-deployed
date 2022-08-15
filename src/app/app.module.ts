1e4710700ae53319ad820047321bd5451deacc49
GrantRT <granto_92@hotmail.com>
2022-08-15 11:56:20 +0100

    adding portfolio code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkComponent } from './components/work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent,
+    SkillsComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule
